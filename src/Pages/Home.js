6ea3582a692614e730e708043aa157cfec4e5630
Vyga <vygintaskirda@gmail.com>
2022-11-28 17:00:43 +0200

    Sustvarkiau CSS

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -20,13 +20,13 @@ const Home = () => {
   return (
     <div className='home-wrapper'>
       <div className={country?'home-side home':"home-mid home"}>
-        <h1>Find a country of interest...</h1>
+        <h1 className={country?'search-title-small':'search-title-big'}>Find a country of interest:</h1>
         <form className='search-form' onSubmit={submitHandler}>
           <input id='countryInput' type="text" placeholder='Enter country name'></input>
           <input type='submit' value='Search' ></input>
         </form>
       </div>
-      {!country ? <h1>Loading…</h1> :
+      {country &&
       <Country name={country} />
       }
   
