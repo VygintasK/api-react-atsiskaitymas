a1360ebfe6e5c1b1a6258121fac083291151e146
Vyga <vygintaskirda@gmail.com>
2022-11-28 17:32:11 +0200

    Replaced hooks with AX

--- src/Pages/CountriesPage.js
+++ src/Pages/CountriesPage.js
@@ -1,32 +1,24 @@
-import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import useAxios from 'axios-hooks'
 import "./CountriesPage.css"
 
 
 const CountriesPage = () => {
-  
-  const [countries, setCountries]= useState([ ])
 
-  useEffect(()=>{
-      fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(countryData =>{
-    
-      setCountries(countryData)
-    })
-  },[])
+  const [{ data, loading, error }] = useAxios(
+    'https://restcountries.com/v3.1/all'
+  )
+  if (loading) return <p className='error-loading'><strong>Loading...</strong></p>
+  if (error) return <p className='error-loading'><strong>Error...</strong></p>
 
   return (
     <div className='countriesWrapper'>
       <h1>WORLDS COUNTRIES: </h1>
-      <div className='test'></div>
-      {countries && (
-        <ul>
-          {countries.map((country,index)=> 
-          <li key={index}><Link to={'/countries/'+country.cca2}>{country.name.common}</Link></li> 
-          )}
-        </ul>
+      <ul>
+        {data.map((country, index) =>
+          <li key={index}><Link to={'/countries/' + country.cca2}>{country.name.common}</Link></li>
         )}
+      </ul>
     </div>
   )
 
