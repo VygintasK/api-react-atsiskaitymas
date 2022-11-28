e69d45c52e777b4c37828e27820143ca9872ac4b
Vyga <vygintaskirda@gmail.com>
2022-11-28 17:43:09 +0200

    Axios hooked

--- src/Pages/CountryPage.js
+++ src/Pages/CountryPage.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import useAxios from 'axios-hooks'
 import { useParams } from 'react-router-dom'
 import Country from '../Components/Country';
 import "./CountryPage.css"
@@ -7,30 +7,16 @@ import "./CountryPage.css"
 const CountryPage = () => {
   let params = useParams();
 
-  const [loading, setLoading] = useState(true);
-  const [country, setCountry] = useState([])
-
-  useEffect(() => {
-
-    async function fetchFunkcijaPerAxios() {
-      const response = await axios.get('https://restcountries.com/v3.1/alpha/' + params.cca2)
-
-      let countryOBJ = response.data[0]
-      setCountry(countryOBJ)
-      setLoading(false)
-    }
-    fetchFunkcijaPerAxios()
-  }, [params.cca2])
+  const [{ data, loading, error }] = useAxios(
+    'https://restcountries.com/v3.1/alpha/' + params.cca2
+  )
+  if (loading) return <p className='error-loading'><strong>Loading...</strong></p>
+  if (error) return <p className='error-loading'><strong>Error...</strong></p>
 
 
   return (
     <div className='country-page-wrapper'>
-      
-      {loading ? <h1>Loading…</h1> :
-        <Country name={country.name.common}></Country>
-      }
-        
-      
+      <Country name={data[0].name.common}></Country>
     </div>
   )
 }
