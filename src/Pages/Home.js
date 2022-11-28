1fdca799785b38a768a4b3664f7d72e36877e0bc
Vyga <vygintaskirda@gmail.com>
2022-11-28 17:17:39 +0200

    Test pusH

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,8 +3,6 @@ import './Home.css'
 
 import Country from '../Components/Country'
 
-
-
 const Home = () => {
 
   const [country, setCountry] = useState('')
@@ -19,17 +17,17 @@ const Home = () => {
 
   return (
     <div className='home-wrapper'>
-      <div className={country?'home-side home':"home-mid home"}>
-        <h1 className={country?'search-title-small':'search-title-big'}>Find a country of interest:</h1>
+      <div className={country ? 'home-side home' : "home-mid home"}>
+        <h1 className={country ? 'search-title-small' : 'search-title-big'}>Find a country of interest:</h1>
         <form className='search-form' onSubmit={submitHandler}>
           <input id='countryInput' type="text" placeholder='Enter country name'></input>
           <input type='submit' value='Search' ></input>
         </form>
       </div>
       {country &&
-      <Country name={country} />
+        <Country name={country} />
       }
-  
+
     </div>
 
   )
